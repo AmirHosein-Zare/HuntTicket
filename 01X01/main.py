db28672f4a420ae687a6999dd2efee3e1088f891
AmirHosein <awir.software@gmail.com>
2023-03-14 00:59:27 +0330

    get elements

--- 01X01/main.py
+++ 01X01/main.py
@@ -1,7 +1,13 @@
+from selenium.webdriver.common.by import By
 from selenium import webdriver
+import time
 
 # set browser
 driver = webdriver.Chrome()
 driver.maximize_window()  
 # get url
 driver.get("https://safar724.com/bus/tehran-shiraz?date=1401/12/25")
+
+elements = driver.find_elements(By.CSS_SELECTOR, '.available-seat>span')
+
+
