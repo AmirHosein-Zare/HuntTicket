47aa4e0f2c9facea3a879cae0795614e8f9d01fa
AmirHosein <awir.software@gmail.com>
2023-03-14 02:16:33 +0330

    add email

--- 01X01/main.py
+++ 01X01/main.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from email import PyEmailer
 
 # set browser
 driver = webdriver.Chrome()
@@ -8,13 +7,12 @@ driver.maximize_window()
 # get url
 driver.get("https://safar724.com/bus/tehran-shiraz?date=1401/12/25")
 
-
-
 for i in range(100000000000000000):
     # get elements
     elements = driver.find_elements(By.CSS_SELECTOR, '.available-seat>span')
     # check amount
     for element in elements:
         if element.text > 0:
+
             print('buy')
             break
