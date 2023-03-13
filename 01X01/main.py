d208a527341edea4bc2cd909cb45e815b341c3e7
AmirHosein <awir.software@gmail.com>
2023-03-14 01:03:22 +0330

    add loop for run every second

--- 01X01/main.py
+++ 01X01/main.py
@@ -8,8 +8,13 @@ driver.maximize_window()
 # get url
 driver.get("https://safar724.com/bus/tehran-shiraz?date=1401/12/25")
 
-elements = driver.find_elements(By.CSS_SELECTOR, '.available-seat>span')
 
-for element in elements:
-    if element.text > 0:
-        print('buy')
+
+for i in range(100000000000000000):
+    # get elements
+    elements = driver.find_elements(By.CSS_SELECTOR, '.available-seat>span')
+    # check amount
+    for element in elements:
+        if element.text > 0:
+            print('buy')
+            break
