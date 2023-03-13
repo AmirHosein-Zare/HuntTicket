903436571d7fee014c4accae8878580e7708fc93
AmirHosein <awir.software@gmail.com>
2023-03-14 00:38:26 +0330

    add url

--- 01X01/main.py
+++ 01X01/main.py
@@ -2,4 +2,6 @@ from selenium import webdriver
 
 # set browser
 driver = webdriver.Chrome()
-
+driver.maximize_window()  
+# get url
+driver.get("https://safar724.com/bus/tehran-shiraz?date=1401/12/25")
