bbbf7904c095a84f28d344528a901da26445a04e
AmirHosein Zare <awir.software@gmail.com>
2023-03-14 13:33:56 +0330

    Update mail.py

--- 01X01/mail.py
+++ 01X01/mail.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 import time
 
-# set browser
-driver = webdriver.Chrome()
-driver.maximize_window()  
-# get url
-driver.get("https://aspb2.asset.aparat.com/aparat-video/7cb8f957bb4c61b5f016b87a8a00439515756123-144p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjU5NTBiNGExNTc3Y2E1MmEyYmM4NWQwZGRjODMyNzhmIiwiZXhwIjoxNjc4ODA1NjI4LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.jEUhGDkgjqxXkwzWZ3svaRQoWz1lcNk44PRejsvdgt4")
+def alarm():
+  # set browser
+  driver = webdriver.Chrome()
+  driver.maximize_window()  
+  # get url
+  driver.get("https://aspb2.asset.aparat.com/aparat-video/7cb8f957bb4c61b5f016b87a8a00439515756123-144p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjU5NTBiNGExNTc3Y2E1MmEyYmM4NWQwZGRjODMyNzhmIiwiZXhwIjoxNjc4ODA1NjI4LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.jEUhGDkgjqxXkwzWZ3svaRQoWz1lcNk44PRejsvdgt4")
 
-time.sleep(60)
+  time.sleep(60)
