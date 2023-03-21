77bb0cf1f5f5fbfe19a1d1e5e9414b30f35c9741
AmirHosein <awir.software@gmail.com>
2023-03-22 02:49:38 +0430

    set choose seat

--- 01X01/chair.py
+++ 01X01/chair.py
@@ -1,7 +1,16 @@
+import time
+
 def choose(driver, By):
-    for i in range(25):
-        chair = driver.find_element(By.CSS_SELECTOR, '#' + i)
-        if chair.get_attribute('class').find('available'):
-            chair.click()
+    chairs = driver.find_elements(By.CLASS_NAME, 'seat')
+    for chair in chairs:
+        print(chair.text)
+        if 'available' in chair.get_attribute('class'):
+            seat = chair.find_element(By.CSS_SELECTOR, 'span')
+            print('before click')
+            seat.click()
+            print('after click')
+            time.sleep(5)
             nextBtn = driver.find_element(By.CSS_SELECTOR, '.nextbutton-panel>div>button')
             nextBtn.click()
+            time.sleep(5)
+            break
