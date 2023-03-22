c2f95563ce127d6e3b46c49eb2d5102faab97883
AmirHosein <awir.software@gmail.com>
2023-03-23 03:47:26 +0430

    delete prints in chair

--- 01X01/chair.py
+++ 01X01/chair.py
@@ -3,12 +3,9 @@ import time
 def choose(driver, By):
     chairs = driver.find_elements(By.CLASS_NAME, 'seat')
     for chair in chairs:
-        print(chair.text)
         if 'available' in chair.get_attribute('class'):
             seat = chair.find_element(By.CSS_SELECTOR, 'span')
-            print('before click')
             seat.click()
-            print('after click')
             time.sleep(5)
             nextBtn = driver.find_element(By.CSS_SELECTOR, '.nextbutton-panel>div>button')
             nextBtn.click()
