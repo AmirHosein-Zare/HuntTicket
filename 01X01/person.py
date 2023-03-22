022b59b464657441ca5369fac5c14df43f70c2e3
AmirHosein <awir.software@gmail.com>
2023-03-23 03:17:22 +0430

    set person info

--- 01X01/person.py
+++ 01X01/person.py
@@ -1,3 +1,7 @@
+user_name = "امیرحسین"
+user_family = "زارع"
+user_mobile = "09172535548"
+user_code = "2520185740"
 
 def input(driver, By):
     mobile = driver.find_element(By.NAME, 'rMobile')
@@ -5,3 +9,5 @@ def input(driver, By):
     name = driver.find_element(By.NAME, 'rName')
     family = driver.find_element(By.NAME, 'rLName')
     btn = driver.find_element(By.CLASS_NAME, 'btn-u-blue')
+
+    
