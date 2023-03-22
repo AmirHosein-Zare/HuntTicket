2ab0a94dbbcbf61166159eabbfc2b975b8b22509
AmirHosein <awir.software@gmail.com>
2023-03-23 03:19:48 +0430

    finish info form and click next

--- 01X01/person.py
+++ 01X01/person.py
@@ -1,3 +1,5 @@
+import time
+
 user_name = "امیرحسین"
 user_family = "زارع"
 user_mobile = "09172535548"
@@ -10,4 +12,11 @@ def input(driver, By):
     family = driver.find_element(By.NAME, 'rLName')
     btn = driver.find_element(By.CLASS_NAME, 'btn-u-blue')
 
-    
+    mobile.send_keys(user_mobile)
+    code.send_keys(user_code)
+    name.send_keys(user_name)
+    family.send_keys(user_family)
+
+    time.sleep(2)
+
+    btn.click()
