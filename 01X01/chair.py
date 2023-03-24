e6ad54046a541b686cefef644047346072eb56b7
AmirHosein <awir.software@gmail.com>
2023-03-24 16:32:21 +0430

    add comment to chair madule

--- 01X01/chair.py
+++ 01X01/chair.py
@@ -1,12 +1,16 @@
 import time
 
 def choose(driver, By):
+    # get all chairs
     chairs = driver.find_elements(By.CLASS_NAME, 'seat')
+    # loop for check available chair
     for chair in chairs:
         if 'available' in chair.get_attribute('class'):
+            # click on available chair
             seat = chair.find_element(By.CSS_SELECTOR, 'span')
             seat.click()
             time.sleep(5)
+            # click to next page
             nextBtn = driver.find_element(By.CSS_SELECTOR, '.nextbutton-panel>div>button')
             nextBtn.click()
             time.sleep(5)
