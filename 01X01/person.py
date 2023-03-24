4e371bb4675ba3dda30ffbe39e30d01a3052bb52
AmirHosein <awir.software@gmail.com>
2023-03-24 16:33:26 +0430

    add comment to person madule

--- 01X01/person.py
+++ 01X01/person.py
@@ -1,17 +1,20 @@
 import time
 
+# personal data
 user_name = "your-name"
 user_family = "your-family"
 user_mobile = "your-mobile"
 user_code = "your-code"
 
 def insert(driver, By):
+    # get inputs
     mobile = driver.find_element(By.NAME, 'rMobile')
     code = driver.find_element(By.NAME, 'rcode')
     name = driver.find_element(By.NAME, 'rName')
     family = driver.find_element(By.NAME, 'rLName')
     btn = driver.find_element(By.CLASS_NAME, 'btn-u-blue')
 
+    # insert to inputs
     mobile.send_keys(user_mobile)
     code.send_keys(user_code)
     name.send_keys(user_name)
@@ -19,4 +22,5 @@ def insert(driver, By):
 
     time.sleep(2)
 
+    # submit data
     btn.click()
